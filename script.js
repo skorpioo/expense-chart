2791fb5587b684536f9968235b429192a0dacb48
Martijn Bulinga <94762243+skorpioo@users.noreply.github.com>
2023-07-01 12:47:58 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ fetch("data.json")
 		for (let day of days) {
 			content += `
       <div class="chart__item">
-         <div class="chart__amount | fs-small fw-bold fc-light" data-amount>${day.amount}</div>
-         <div class="chart__bar" style="height:calc(${day.amount}px * 2.25)"></div>
+         <div class="chart__amount | fs-xsmall fw-bold fc-light" data-amount>${day.amount}</div>
+         <div class="chart__bar" style="height:calc(${day.amount}px * 2.865)"></div>
          <div class="chart__day | fs-xsmall fc-medium">${day.day}</div>
       </div>`;
 		}
